faeddba3d0d83717ffff1c42be22d088712b6a75
Will Homan <will.homan@hotmail.com>
2017-04-15 11:46:41 -0700

    CORS

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ var express = require("express");
 var app = express();
 var fs = require("fs");
 
-app.get('/', function(request, response) {
-  response.send("<h1>Hello Express</h1>");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  res.send("<h1>Welcome to my Simple API</h1>");
 })
 
-app.get('/getdata', function (req, res) {
+app.get('/getdata', function (req, res, next) {
    fs.readFile( __dirname + "/" + "resumeData.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
